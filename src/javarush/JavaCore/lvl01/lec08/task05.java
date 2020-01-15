a94fa0d50a5917bd423d391836ed9a38fe174f69
VadHead <vadhead@gmail.com>
2020-01-15 16:41:00 +0200

    Core

---
+++ src/javarush/JavaCore/lvl01/lec08/task05.java
@@ -0,0 +1,42 @@
+package javarush.JavaCore.lvl01.lec08;
+/*taskKey="com.javarush.task.task11.task1110"\n\nНе забываем инкапсулировать
+Посмотри внимательно на методы и добавь недостающие поля.
+Требования:
+1.	В классе Cat должно быть поле типа String с именем name.
+2.	В классе Cat должно быть поле типа int с именем age.
+3.	В классе Cat должно быть поле типа int с именем weight.
+4.	В классе Cat должно быть поле типа int с именем speed.
+5.	В классе Cat должны быть 4 поля.
+6.	Все поля класса Cat должны быть private.*/
+public class task05 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        private int age;
+        private String name;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
