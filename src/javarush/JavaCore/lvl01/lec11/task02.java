9902937f65f690585cf16ca315ce4ebe99e974f5
VadHead <vadhead@gmail.com>
2020-01-17 14:19:24 +0200

    Core

---
+++ src/javarush/JavaCore/lvl01/lec11/task02.java
@@ -0,0 +1,23 @@
+package javarush.JavaCore.lvl01.lec11;
+/*taskKey="com.javarush.task.task11.task1112"\n\nЗаклятые друзья
+Изменить два класса AppleIPhone и SamsungGalaxyS2.
+Унаследовать SamsungGalaxyS2 от AppleIPhone.
+Требования:
+1.	В классе Solution должен быть класс AppleIPhone.
+2.	Класс AppleIPhone должен быть public.
+3.	В классе Solution должен быть класс SamsungGalaxyS2.
+4.	Класс SamsungGalaxyS2 должен быть public.
+5.	Класс SamsungGalaxyS2 должен быть унаследован от класса AppleIPhone.*/
+public class task02 {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone {
+
+
+    }
+}
---
+++ src/javarush/JavaCore/lvl01/lec11/task08.java
@@ -0,0 +1,32 @@
+package javarush.JavaCore.lvl01.lec11;
+/*taskKey="com.javarush.task.task11.task1118"\n\nГородское хозяйство
+Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот), Dog (собака), Car (машина).
+Требования:
+1.	В классе Solution должен быть public класс Pet (домашнее животное).
+2.	В классе Solution должен быть public класс Cat (кот).
+3.	В классе Solution должен быть public класс Dog (собака).
+4.	В классе Solution должен быть public класс Car (машина).
+5.	Правильно унаследуй класс Cat.
+6.	Правильно унаследуй класс Dog.
+7.	Правильно унаследуй класс Pet.
+8.	Правильно унаследуй класс Car.*/
+public class task08 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet{
+
+    }
+}
